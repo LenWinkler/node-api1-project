3b40ef02fbb619ece4d6f570275ba6dccdfbfddd
LenWinkler <lenwinkler86@gmail.com>
2019-10-28 19:56:49 -0400

    added PUT endpoint to update user

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ server.post('/api/users', (req, res) => {
             res.status(500).json({ error: "There was an error while saving the user to the database" })
         })
     }
-})
+});
 
 server.get('/api/users', (req, res) => {
     users.find()
@@ -51,24 +51,44 @@ server.get('/api/users/:id', (req, res) => {
             console.log('find by id error', err);
             res.status(500).json({ error: "The user information could not be retrieved" })
         })
-})
+});
 
 server.delete('/api/users/:id', (req, res) => {
     const id = req.params.id;
 
     users.remove(id)
         .then(del => {
-            if(del === 0) {
-                res.status(404).json({ message: "The user with the specified id does not exist" })
-            } else {
+            if(del > 0) {
                 res.json({ message: "User successfully removed" })
+            } else {
+                res.status(404).json({ message: "The user with the specified id does not exist" })
             }
         })
         .catch(err => {
             console.log('delete error', err);
             res.status(500).json({ error: "User could not be removed" })
         })
-})
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const newInfo = req.body;
+
+    users.update(id, newInfo)
+        .then(updated => {
+            if(updated === 0) {
+                res.status(404).json({ message: "The user with the specified id does not exist" })
+            } else if(!newInfo.name || !newInfo.bio) {
+                res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+            } else {
+                res.status(200).json(req.body)
+            }
+        })
+        .catch(err => {
+            console.log('update error', err);
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+});
 
 
 const port = 8000;
