e7b5b01fb4b5c2fe010d23b6ba91c2978a0fd217
LenWinkler <lenwinkler86@gmail.com>
2019-10-28 17:11:25 -0400

    added POST to add user

--- index.js
+++ index.js
@@ -5,6 +5,27 @@ const server = express();
 
 const users = require('./data/db');
 
+server.use(express.json());
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    if(!newUser.name || !newUser.bio) {
+        res.status(400).json({ errorMessage: "Please provide both name and bio for the user." })
+    } else {
+
+    users.insert(newUser)
+        .then(newUsr => {
+            console.log(newUser)
+            res.status(201).json(newUsr)
+        })
+        .catch(err => {
+            console.log('user add error', err)
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+    }
+})
+
 server.get('/api/users', (req, res) => {
     users.find()
         .then(users => {
@@ -16,7 +37,7 @@ server.get('/api/users', (req, res) => {
         })
 });
 
-server.get
+
 
 const port = 8000;
 server.listen(port, () => console.log('\nAPI on port 8000\n'));
